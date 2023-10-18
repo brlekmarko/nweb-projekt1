b2f1dad60e1077d98bf8859e452f5475c07d0831
Marko Brlek <marko.brlek@fer.hr>
2023-10-18 17:29:50 +0200

    Created a basic home page and not found page

---
+++ src/pages/homePage/homePage.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+    return (
+        <div className="home-page-center">
+            This is a home page
+        </div>
+    );
+};
+
+export default HomePage;
---
+++ src/pages/notFoundPage/notFoundPage.tsx
@@ -0,0 +1,9 @@
+const NotFoundPage = () => {
+    return (
+        <div className="not-found-center">
+            404 This page does not exist
+        </div>
+    );
+};
+
+export default NotFoundPage;
